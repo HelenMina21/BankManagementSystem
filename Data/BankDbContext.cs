5e7142f9cda62c4c84caaeac046e24308a96d079
HelenMina21 <helomina1@gmail.com>
2026-08-25 13:30:00 +0300

    DataSeeding

--- Data/BankDbContext.cs
+++ Data/BankDbContext.cs
@@ -20,6 +20,7 @@ namespace BankManagementSystem.Data
         public DbSet<Account> Accounts { get; set; }
         public DbSet<Customer> Customers { get; set; }
         public DbSet<Transaction> Transactions { get; set; }
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Branch>(entity =>
@@ -75,11 +76,11 @@ namespace BankManagementSystem.Data
             }
             );
 
-            modelBuilder.Entity<Transaction>(entity=>
+            modelBuilder.Entity<Transaction>(entity =>
             {
                 entity.HasKey(t => t.TransactionNumber);
                 entity.Property(t => t.TransactionType).IsRequired();
-                entity.Property(t => t.Amount).HasPrecision(18,2).IsRequired();
+                entity.Property(t => t.Amount).HasPrecision(18, 2).IsRequired();
                 entity.Property(t => t.TransactionDate).IsRequired();
                 entity.Property(t => t.Note).IsRequired();
             }
@@ -112,6 +113,148 @@ namespace BankManagementSystem.Data
                .HasOne(ca => ca.Account)
                .WithMany(a => a.CustomerAccounts)
                .HasForeignKey(ca => ca.AccountNumber);
+
+
+            //DATA SEEDING
+            modelBuilder.Entity<Branch>().HasData(
+    new Branch
+    {
+        Code = "B001",
+        Name = "Main Branch",
+        Address = "Cairo",
+        PhoneNumber = "01000000000"
+    },
+    new Branch
+    {
+        Code = "B002",
+        Name = "Nasr City Branch",
+        Address = "Nasr City",
+        PhoneNumber = "01100000000"
+    }
+);
+
+            modelBuilder.Entity<Manager>().HasData(
+    new Manager
+    {
+        Id = 1,
+        FullName = "Ahmed Hassan",
+        Email = "ahmed.hassan@bank.com",
+        PhoneNumber = "01011111111",
+        HireDate = new DateTime(2020, 1, 15),
+        BranchCode = "B001"
+    },
+    new Manager
+    {
+        Id = 2,
+        FullName = "Sara Mohamed",
+        Email = "sara.mohamed@bank.com",
+        PhoneNumber = "01022222222",
+        HireDate = new DateTime(2021, 6, 10),
+        BranchCode = "B002"
+    }
+);
+
+            modelBuilder.Entity<Customer>().HasData(
+    new Customer
+    {
+        Id = 1,
+        FullName = "Mohamed Ali",
+        Email = "mohamed.ali@email.com",
+        CustomerType = "Individual",
+        Address = "Cairo",
+        NationalId = "29505201234567",
+        PhoneNumber = "01033333333",
+        DateOfBirth = new DateTime(1995, 5, 20)
+
+    },
+    new Customer
+    {
+        Id = 2,
+        FullName = "Mariam Adel",
+        Email = "mariam.adel@email.com",
+        CustomerType = "Individual",
+        Address = "Giza",
+        NationalId = "29808121234567",
+        PhoneNumber = "01044444444",
+        DateOfBirth = new DateTime(1998, 8, 12)
+    }
+);
+
+            modelBuilder.Entity<Account>().HasData(
+    new Account
+    {
+        AccountNumber = "ACC001",
+        AccountType = "Savings",
+        Balance = 15000.00m,
+        OpeningDate = new DateTime(2023, 1, 10),
+        BranchCode = "B001"
+    },
+    new Account
+    {
+        AccountNumber = "ACC002",
+        AccountType = "Current",
+        Balance = 25000.00m,
+        OpeningDate = new DateTime(2023, 5, 15),
+        BranchCode = "B002"
+    }
+);
+
+            modelBuilder.Entity<CustomerAccount>().HasData(
+    new CustomerAccount
+    {
+        CustomerId = 1,
+        AccountNumber = "ACC001",
+        OwnershipType = "Primary",
+        OwnershipStartDate = new DateTime(2023, 1, 10),
+        AccountStatus = "Active"
+    },
+    new CustomerAccount
+    {
+        CustomerId = 1,
+        AccountNumber = "ACC002",
+        OwnershipType = "Primary",
+        OwnershipStartDate = new DateTime(2023, 5, 15),
+        AccountStatus = "Active"
+    },
+    new CustomerAccount
+    {
+        CustomerId = 2,
+        AccountNumber = "ACC002",
+        OwnershipType = "CoHolder",
+        OwnershipStartDate = new DateTime(2023, 5, 15),
+        AccountStatus = "Active"
+    }
+);
+
+            modelBuilder.Entity<Transaction>().HasData(
+    new Transaction
+    {
+        TransactionNumber = 1,
+        TransactionDate = new DateTime(2023, 2, 15),
+        Note = "Initial deposit",
+        TransactionType = "Deposit",
+        Amount = 5000.00m,
+        AccountNumber = "ACC001"
+    },
+    new Transaction
+    {
+        TransactionNumber = 2,
+        TransactionDate = new DateTime(2023, 6, 1),
+        Note = "ATM withdrawal",
+        TransactionType = "Withdrawal",
+        Amount = 2000.00m,
+        AccountNumber = "ACC002"
+    },
+    new Transaction
+    {
+        TransactionNumber = 3,
+        TransactionDate = new DateTime(2023, 6, 10),
+        Note = "Monthly payment",
+        TransactionType = "Payment",
+        Amount = 1000.00m,
+        AccountNumber = "ACC002"
+    }
+);
         }
 
     }
---
+++ Migrations/20260825102105_AddInitialSeedData.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace G_NET_5_EF04.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddInitialSeedData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Branches",
+                columns: new[] { "Code", "Address", "Name", "PhoneNumber" },
+                values: new object[,]
+                {
+                    { "B001", "Cairo", "Main Branch", "01000000000" },
+                    { "B002", "Nasr City", "Nasr City Branch", "01100000000" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Customers",
+                columns: new[] { "Id", "Address", "CustomerType", "DateOfBirth", "Email", "FullName", "NationalId", "PhoneNumber" },
+                values: new object[,]
+                {
+                    { 1, "Cairo", "Individual", new DateTime(1995, 5, 20, 0, 0, 0, 0, DateTimeKind.Unspecified), "mohamed.ali@email.com", "Mohamed Ali", "29505201234567", "01033333333" },
+                    { 2, "Giza", "Individual", new DateTime(1998, 8, 12, 0, 0, 0, 0, DateTimeKind.Unspecified), "mariam.adel@email.com", "Mariam Adel", "29808121234567", "01044444444" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Accounts",
+                columns: new[] { "AccountNumber", "AccountType", "Balance", "BranchCode", "OpeningDate" },
+                values: new object[,]
+                {
+                    { "ACC001", "Savings", 15000.00m, "B001", new DateTime(2023, 1, 10, 0, 0, 0, 0, DateTimeKind.Unspecified) },
+                    { "ACC002", "Current", 25000.00m, "B002", new DateTime(2023, 5, 15, 0, 0, 0, 0, DateTimeKind.Unspecified) }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Managers",
+                columns: new[] { "Id", "BranchCode", "Email", "FullName", "HireDate", "PhoneNumber" },
+                values: new object[,]
+                {
+                    { 1, "B001", "ahmed.hassan@bank.com", "Ahmed Hassan", new DateTime(2020, 1, 15, 0, 0, 0, 0, DateTimeKind.Unspecified), "01011111111" },
+                    { 2, "B002", "sara.mohamed@bank.com", "Sara Mohamed", new DateTime(2021, 6, 10, 0, 0, 0, 0, DateTimeKind.Unspecified), "01022222222" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "CustomerAccount",
+                columns: new[] { "AccountNumber", "CustomerId", "AccountStatus", "OwnershipStartDate", "OwnershipType" },
+                values: new object[,]
+                {
+                    { "ACC001", 1, "Active", new DateTime(2023, 1, 10, 0, 0, 0, 0, DateTimeKind.Unspecified), "Primary" },
+                    { "ACC002", 1, "Active", new DateTime(2023, 5, 15, 0, 0, 0, 0, DateTimeKind.Unspecified), "Primary" },
+                    { "ACC002", 2, "Active", new DateTime(2023, 5, 15, 0, 0, 0, 0, DateTimeKind.Unspecified), "CoHolder" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Transactions",
+                columns: new[] { "TransactionNumber", "AccountNumber", "Amount", "Note", "TransactionDate", "TransactionType" },
+                values: new object[,]
+                {
+                    { 1, "ACC001", 5000.00m, "Initial deposit", new DateTime(2023, 2, 15, 0, 0, 0, 0, DateTimeKind.Unspecified), "Deposit" },
+                    { 2, "ACC002", 2000.00m, "ATM withdrawal", new DateTime(2023, 6, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Withdrawal" },
+                    { 3, "ACC002", 1000.00m, "Monthly payment", new DateTime(2023, 6, 10, 0, 0, 0, 0, DateTimeKind.Unspecified), "Payment" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "CustomerAccount",
+                keyColumns: new[] { "AccountNumber", "CustomerId" },
+                keyValues: new object[] { "ACC001", 1 });
+
+            migrationBuilder.DeleteData(
+                table: "CustomerAccount",
+                keyColumns: new[] { "AccountNumber", "CustomerId" },
+                keyValues: new object[] { "ACC002", 1 });
+
+            migrationBuilder.DeleteData(
+                table: "CustomerAccount",
+                keyColumns: new[] { "AccountNumber", "CustomerId" },
+                keyValues: new object[] { "ACC002", 2 });
+
+            migrationBuilder.DeleteData(
+                table: "Managers",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Managers",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Transactions",
+                keyColumn: "TransactionNumber",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Transactions",
+                keyColumn: "TransactionNumber",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Transactions",
+                keyColumn: "TransactionNumber",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Accounts",
+                keyColumn: "AccountNumber",
+                keyValue: "ACC001");
+
+            migrationBuilder.DeleteData(
+                table: "Accounts",
+                keyColumn: "AccountNumber",
+                keyValue: "ACC002");
+
+            migrationBuilder.DeleteData(
+                table: "Customers",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Customers",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Branches",
+                keyColumn: "Code",
+                keyValue: "B001");
+
+            migrationBuilder.DeleteData(
+                table: "Branches",
+                keyColumn: "Code",
+                keyValue: "B002");
+        }
+    }
+}
